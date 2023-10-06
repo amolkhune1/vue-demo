2b21a308e5fe4bc42979e2a3d2c4c8cb6d4f75ec
Amol <akhune1@gmail.com>
2023-10-06 09:26:13 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <GameCard amount="6"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import GameCardVue from './components/GameCard.vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    GameCard:GameCardVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GameCard.vue
@@ -0,0 +1,53 @@
+<template>
+<table>
+    <tbody>
+        <tr :key="index"  class="margin" v-for="(cardTitle,index) in cardsArray">
+            <td  v-on:click="changeText(index)"  >
+                {{ cardTitle }}
+            </td>
+        </tr>
+    </tbody>
+</table>
+</template>
+<script>
+export default{
+    name:'CardsComponent',
+    data(){
+        return{
+            cardTitle:'down',
+            cardsArray:[],
+            otherTitle:'up',
+            previousIndex:null
+        }
+    },
+    created:function(){
+    this.calculateCards();
+   },
+    props:{
+        amount:String,
+    },
+    methods:{
+        calculateCards(){
+            for(let i=0;i<this.amount;i++){
+                this.cardsArray.push(this.cardTitle);
+            }
+        },
+        changeText(index){
+            if(this.cardsArray[index]==this.cardTitle){
+                if(this.previousIndex!=null){
+                    this.cardsArray[this.previousIndex]=this.cardTitle;
+                }
+                this.cardsArray[index]=this.otherTitle;
+                this.previousIndex=index;
+            }
+            console.log(this.cardsArray);
+        }
+    }
+}
+</script>
+<style>
+.margin{
+    margin: 20px;
+    border: 1px solid;
+}
+</style>
